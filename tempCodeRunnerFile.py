b1980f58c1263cfbca903a768e04cda2e2334409
ariake <kyh31876@gmail.com>
2021-04-19 17:04:06 +0900

    commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-print(idnumber[:2])
+names =[] 
+name_str = input("다섯명 이름 입력 :") 
+name_str.split()
+
+
+rmv=input("삭제할 이름입력 :")
+if rmv in names:
+    names.remove(rmv)
+    print("%s가작세되었습니다." %rmv)
+else: 
+    print("%s가 존재하지 않습니다." %rmv)
